d0234ca3fd1987911f040dc979c90b71869c8b04
luojie1 <luojie1@ruijie.com.cn>
2023-10-24 00:25:52 +0800

    调整布局和部分跳转内容

---
+++ src/views/analysis/analysis_overview.vue
@@ -0,0 +1,44 @@
+/**
+ * @FileDesc: 数据库分析概览
+ * @Author:
+ * @Func: 能够进行数据分析概览
+ * @Components:
+ */
+<template>
+  <BasicLayout>
+    <template #wrapper>
+      <div class="box-card">
+        <div class="report-title">
+          <h2 class="title">
+            分析管理
+          </h2>
+        </div>
+        <div class="report-index">
+          <div class="report-index">
+            <h4 class="item">
+              <span style="font-size:16px;">分析导航</span>
+            </h4>
+            <div class="item">
+              <p>
+                分析导航提供微生物多样性数据分析整体导览图，可通过导览图定制分析流程；分析指南通过真实的文献分析案例引导您快速开启微生物多样性数据分析之旅。
+              </p>
+            </div>
+          </div>
+          <div class="report-index">
+            <h4 class="item">
+              <span style="font-size:16px;">分析软件信息</span>
+            </h4>
+            <div class="item">
+              <p>
+                展示该项目下各分析所用分析软件及其版本号等信息。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div></template>
+  </basiclayout>
+</template>
+<style scoped>
+.report-title{background:#F5F5F5;margin-bottom: 20px;}
+.report-index{border:1px solid #e2e2e2;padding:15px 25px;background: #fff;margin-bottom: 20px;}
+</style>
--- src/views/dashboard/database/index.vue
+++ src/views/dashboard/database/index.vue
@@ -25,7 +25,20 @@
             </el-col>
           </div>
         </el-row>
-        <el-col :span="24">
+        <el-row>
+          <div class="twelve_col last">
+            <h1>欢迎使用 环境DNA数据库管理系统</h1>
+            <p>The National Center for Biotechnology Information
+              advances science and health by providing access to
+              biomedical and genomic information.</p>
+            <el-row>
+              <ul class="about_links">
+                <li><a href="/about/">About the NCBI</a> </li>
+              </ul>
+            </el-row>
+          </div>
+        </el-row>
+        <el-col :span="14">
           <el-row :gutter="5">
             <el-col v-for="(card, index) in cards" :key="index" :span="8">
               <CustomCard :titlename="card.titlename" :description="card.description" :button-title="card.buttonTitle" :img-url="card.imgUrl" :page-url="card.pageUrl" />
@@ -49,12 +62,12 @@ export default {
       // 遮罩层
       loading: true,
       cards: [
-        { titlename: 'NCBI数据库', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/database/search_ncbi' },
-        { titlename: '北京数据库', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/database/search_ncbi' },
-        { titlename: '物种分类  ', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/taxonomy/index' },
-        { titlename: '上传', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/database/update' },
-        { titlename: '管理', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/taxonomy/index' },
-        { titlename: 'DNA分析', description: 'Deposit data or manuscripts into NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/analysis/analysis_overview' }
+        { titlename: 'NCBI数据库', description: 'NCBI databases', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/database/search_ncbi' },
+        { titlename: '北京数据库', description: '北京本地数据库', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/database/search_ncbi' },
+        { titlename: '物种分类  ', description: '数据库物种信息分类', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/taxonomy/index' },
+        { titlename: '上传', description: '数据库上传', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/dashboard/database/update' },
+        { titlename: '管理', description: '数据库系统管理', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/analysis/analysis_overview' },
+        { titlename: 'DNA分析', description: '根据项目进行DNA分析', imgUrl: require('@/assets/logo/logo.png'), pageUrl: '/analysis/analysis_overview' }
       ],
       selectedOption: 'all',
       searchText: ''
@@ -65,7 +78,7 @@ export default {
       // 处理搜索功能，根据选项和输入内容执行相应的搜索逻辑
       console.log('执行搜索：选项 -', this.selectedOption, '关键词 -', this.searchText)
       //
-      this.$router.push('/dashboard/database/search_ncbi')
+      this.$router.push('/database/search_ncbi')
     },
     open() {
       this.$alert('数据库搜索使用NCBI或者北京本地数据库', '使用帮助', {
@@ -95,5 +108,9 @@ export default {
   padding: 14px 0 10px 0;
   display: block;
   font-family: "Roboto", "Helvetica Neue", Arial, Tahoma;
+}
+.about_links {
+  padding: 0;
+  margin: 1em 0 1em 1em
 }
 </style>
